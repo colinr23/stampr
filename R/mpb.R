e5d60d31cf50bd9c2b77131b5cdf21bf30ec9418
colinr23 <colinr23@gmail.com>
2015-11-12 18:23:39 -0500

    add multichange function, mpb data

---
+++ R/mpb.R
@@ -0,0 +1,21 @@
+#' MPB dataset
+#'
+#' A dataset containing forest fire polygons representing the movement of the XX forest fire from T1 (fire1) to T2 (fire2). The extent of the fire on T1 was xx ha across yy polygons. By T2 the fire extent has increased/decreased to xx ha across yy polygons.
+#' 
+#' The \code{mpb} dataset contains polygons that were derived from XX imagery... more info. the dataset. 
+#'
+#' @references
+#' Some References go here
+#'  
+#' @source To be written
+#'
+#' @docType data
+#' @keywords datasets
+#' @format \code{mpb} --- a \code{SpatialPolygonsDataFrame} with 33 records of of the location of forest fire. \code{fire2} a.. 
+#' @name fire1
+#' @examples
+#' library(sp)
+#' data(mpb)
+#' plot(mpb)
+#' plot(mpb,border=2,add=TRUE)
+NULL
